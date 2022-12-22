5dea9a41c96e5139598f3888ab81604359e2e969
miccraig15 <miccraig15@gmail.com>
2022-12-22 16:56:02 -0600

    Initial Commit

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            const container = document.getElementById("container");
+            let index = 0
+            container.addEventListener("click", function() {
+                container.innerHTML = `
+                    <div class="astronaut">
+                            <h3>${json[index.firstName]} ${json[index.lastName]}
+                            <ul>
+                                <li>Hours in space: ${json[index.hoursInSpace]}</li>
+                                <li>Active: ${json[index.active]}</li>
+                                <li>Skills: ${json[index.skills]}</li>
+                            </ul>
+                        <img class="avatar" src=${json[index.picture]}>
+                    <div/>
+                    `;
+                    index = (index + 1) % json.length;
+            });
+        });
+    });
+});
